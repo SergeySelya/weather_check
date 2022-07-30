ea5e111f721b9f13e3b1c70a83be92de1bb0ba2b
sergo <elhom99@gmail.com>
2022-07-30 14:37:24 +0300

    add weather marker2

--- weather/views.py
+++ weather/views.py
@@ -4,6 +4,8 @@ import requests
 
 def index(request):
     appid = '8450022c6e5a6624d730152e02d959dc'
-    url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid=" +appid
+    url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=" +appid
     city = 'London'
+    res = requests.get(url.format(city))
+    print(res.text)
     return render(request, 'weather/index.html')
