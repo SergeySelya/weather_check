7b1ed99f0607fba92db7b5d217875fc5022b3091
sergo <elhom99@gmail.com>
2022-07-30 14:31:44 +0300

    add weather marker

--- weather/views.py
+++ weather/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+import requests
+
 
 def index(request):
+    appid = '8450022c6e5a6624d730152e02d959dc'
+    url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid=" +appid
+    city = 'London'
     return render(request, 'weather/index.html')
